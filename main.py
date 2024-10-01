9d5d4678c8135c4d4d6350cf47ecab97842375f4
Marceau <marceaupatu123@icloud.com>
2024-10-01 18:22:35 +0200

    Quelques changements esthétiques

--- main.py
+++ main.py
@@ -265,4 +265,4 @@ def delete_user(user_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=3839)
